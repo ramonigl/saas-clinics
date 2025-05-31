e0c87640faa204f76653c4c3e38fc1ebfdc62666
Ramon Iglesias <ramonigl@MacBook-Air-de-Ramon.local>
2025-05-31 00:57:20 -0300

    feat: add session management

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,30 @@
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import SignOutButton from "./components/sign-out-button";
+
 const DashboardPage = async () => {
-  return <div>Dashboard</div>;
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>{session?.user?.name}</p>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.id}</p>
+      <p>{session?.user?.image}</p>
+      <p>{session?.user?.createdAt?.toISOString()}</p>
+      <p>{session?.user?.updatedAt?.toISOString()}</p>
+      <p>{session?.user?.emailVerified ? "true" : "false"}</p>
+      <SignOutButton />
+    </div>
+  );
 };
 
 export default DashboardPage;
